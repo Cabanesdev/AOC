e15d044d92c1771f467f7cd5081c72e46993ba87
Mark Coronado Cabanes <cabanesdev@gmail.com>
2022-12-19 20:32:52 +0100

    ts(): day 1

---
+++ typescript/src/day1.ts
@@ -0,0 +1,30 @@
+import { readFileSync } from 'fs';
+
+function getCalories(): number[] {
+  const calories = readFileSync('../input/day1', 'utf-8')
+    .split('\n\n')
+    .map((values) => values.split('\n'))
+    .map((values) => values.map((value) => Number(value)))
+    .map((values) => values.reduce((a, b) => a + b)).sort((a, b) => a - b).reverse();
+
+  return calories;
+}
+
+function part1(): void {
+  console.log('Part 1');
+  const calories = getCalories();
+  console.log(`Calories me daddy: ${calories[0]}`);
+}
+
+function part2(): void {
+  console.log('Part 2');
+  const calories = getCalories().slice(0, 3).reduce((a, b) => a + b);
+  console.log(`Calories me daddy: ${calories}`);
+}
+
+function main(): void {
+  part1();
+  part2();
+}
+
+main();
